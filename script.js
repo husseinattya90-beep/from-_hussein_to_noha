c13bd7c9ea70b463db24899b246eff65140f4255
husseinattya90-beep <husseinattya90@gmail.com>
2026-03-20 07:37:38 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
-function startDig() {
-
-  const girl = document.querySelector(".girl");
-  const hole = document.querySelector(".hole");
-  const boy = document.querySelector(".boy");
-  const message = document.querySelector(".message");
-
-  // تبدأ تحفر
-  girl.classList.add("dig");
-
-  // تظهر الحفرة
-  setTimeout(() => {
-    hole.classList.add("show");
-  }, 1500);
-
-  // يطلع الولد
-  setTimeout(() => {
-    girl.classList.remove("dig");
-    boy.classList.add("show");
-  }, 3000);
-
-  // تظهر الرسالة
-  setTimeout(() => {
-    message.classList.add("show");
-  }, 4000);
-}
+setTimeout(() => {
+  const guy = document.getElementById("guy");
+  guy.style.opacity = "1";
+  guy.style.transform = "translateY(0)";
+}, 2000);
